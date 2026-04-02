e07015d969322b0bf27878f2e222e759f70859be
Olle-Langwagen <olle.langwagen@gmail.com>
2026-04-02 18:15:39 +0200

    harpoon extra keybnds

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -16,5 +16,15 @@ return {
         keymap("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon fil 2" })
         keymap("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon fil 3" })
         keymap("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon fil 4" })
+        keymap("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon fil 5" })
+        keymap("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "Harpoon fil 6" })
+        keymap("n", "<leader>7", function() harpoon:list():select(7) end, { desc = "Harpoon fil 7" })
+        keymap("n", "<leader>8", function() harpoon:list():select(8) end, { desc = "Harpoon fil 8" })
+        keymap("n", "<leader>9", function() harpoon:list():select(9) end, { desc = "Harpoon fil 9" })
+
+
+
+
+
     end
 }
